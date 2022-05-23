fbbb11d817ecf90bf5b780f10e008c4318906a56
elidvenega <venegaelid@gmail.com>
2022-05-22 19:31:55 -0700

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import Board from "./components/Board";
+import Info from "./components/Info";
 
-import './App.css';
-import Game from './components/Game';
+import "./css/app.css";
+
+import { useState } from "react";
 
 function App() {
+  const [reset, setReset] = useState(false);
+  const [winner, setWinner] = useState("");
+
+  const resetBoard = () => {
+    setReset(true);
+  };
   return (
     <div className="App">
-     <Game />
+      <div className={`winner ${winner !== "" ? "" : "shrink"}`}>
+        <div className="winner-text">{winner}</div>
+        <button onClick={() => resetBoard()}>Reset Boards</button>
+      </div>
+      <Board
+        reset={reset}
+        setReset={setReset}
+        winner={winner}
+        setWinner={setWinner}
+      />
+      <Info />
     </div>
   );
 }
